ef06b813cc5c6c61729a9119a6bca04a419b30e4
alisakolot <kolot.alisa@gmai..com>
2022-03-28 20:28:12 -0500

    add - chai testing

--- tests/firstTest.js
+++ tests/firstTest.js
@@ -1,4 +1,7 @@
 const {Builder, By, Key} = require ("selenium-webdriver");
+const assert = require("assert"); 
+var should = require("chai").should();
+
 
 async function example() {
 
@@ -15,9 +18,29 @@ await driver.get("https://lambdatest.github.io/sample-todo-app/")
 // sampletodotext, id used to select 
 await driver.findElement(By.id("sampletodotext")).sendKeys("Learn Selenium", Key.RETURN);
 
+// assert
+let todoText = await driver.findElement(By.xpath("//li[last()]")).getText().then(function(value){
+    return value
+});
+
+
+//assert using node assertions
+// assert.strictEqual(todoText, "Learn Selenium");
+
+// strictEqual: checks that two strings match
+// 1. what actual val is
+// 2. what second val is
+
+
+//assert using chai should 
+    // chair\ assertions are more popular because they are
+    // more human readable
+
+todoText.should.equal("Learn Selenium");
+
 
 // close the browser
-await driver.quit();
+await driver.quit( );
 
 }
 
